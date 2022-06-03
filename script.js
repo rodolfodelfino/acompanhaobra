f201cd18100b0e6483a5277180b4fadd0751404a
rodolfodelfino <rodolfodelfino@hotmail.com>
2022-06-03 15:01:22 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var num1 = window.document.getElementById('n1')
+var n1 = Number(num1.value)
+
+
+function mudaDiv(){
+var num1 = window.document.getElementById('n1')
+var num2 = window.document.getElementById('n2')
+var n1 = Number(num1.value)
+var img = document.querySelector("#img_prog1");
+img.setAttribute('src', 'img/imagem progresso5.jpg');
+   
+   
+    // container1.innerHTML = `o resultado é ${res} `;
+    // container1.style.width="200px";
+    // container1.style.margin="10px";
+    // // container1.style.backgroundColor="#3fb8af";
+    // container1.style.color="white";
+    // container1.style.padding="80px";
+    // container1.style.alignItens="center";
+    // container1.style.textAlign="center";
+    // container1.style.border="1px solid black";
+    // container1.style.borderRadius="5%";
+    // container1.style.boxShadow="4px 4px 18px rgba(0,0,0,0.5)";
+    // // container1.style.backgroundImage= "url("img\ t1 chip.jpg ")";
+    // document.getElementById("container1").src = "img\ t1 chip.jpg";
+}
+
+
+
